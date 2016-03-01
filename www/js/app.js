05db8525dd9ab830e528baa48612ff9dc676a355
D Thiers <dthiers@student.avans.nl>
2016-03-01 18:09:01 +0100

    moved from angularJS repo to this one.

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $templateCache) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -20,5 +20,18 @@ angular.module('starter', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    $rootScope.$on('$viewContentLoaded', function(){
+      $templateCache.removeAll();
+    });
   });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'js/partials/home-partial.html'
+  })
+
 })
---
+++ www/js/services/testService.js
@@ -0,0 +1,9 @@
+application.service('testService', function(){
+
+// API calls 
+
+this.alert = function(){
+  alert("alert from testService!");
+}
+
+});
